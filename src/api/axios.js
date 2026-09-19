9d71c025667bec2220fe37276f5dc68ee92bf93c
Dareppa Bisanal <serbisyostechnologies@gmail.com>
2026-09-20 04:07:00 +0530

    Changed to production api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import config from './environment';
 
 const api = axios.create({
-  //baseURL: config.VITE_API_BASE_URL,
-  baseURL: "http://172.20.10.2:8000/api/v1",
+  baseURL: config.VITE_API_BASE_URL,
+  //baseURL: "http://172.20.10.2:8000/api/v1",
 
   headers: {
     "Content-Type": "application/json",
